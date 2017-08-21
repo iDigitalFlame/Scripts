5ed10b1cd36eccfb91feeef1328b0a3a26be2a41
iDigitalFlame <idf@idfla.me>
2017-08-21 10:51:47 -0400

    Create motivate.py

---
+++ Python/motivate.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+
+# Protip: Add to .*rc file for extra motivation!
+
+import random
+
+MESSAGES = [
+ 'The only thing stopping you from sucess, is yourself!',
+ '615 SWOLE IS THE GOAL, SIZE IS THE PRIZE, IT\'S GAINS O\'CLOCK MOTHERFUCKER LETS GO!!',
+ 'Fuck all of your excuses! Sit the fuck down and do your work!',
+ 'Do something today that your future self will thank you for!',
+ 'Never wait for things to happen, make them happen for yourself, through hard graft and not giving up!',
+ 'Don\'t chase people, be an example, work hard and be yourself, the people who belong will come and find you, just do your thing!',
+ 'If you want it, work for it. It\'s that simple.',
+ 'On the other side of fear, lies freedom.',
+ 'Sucess is the result of continuous and persistant effort.',
+ 'If you get tired, learn to rest, not to quit.',
+ 'If you don\'t sacrifice for what you want, what you want becomes the sacrifice.',
+ 'You have survived every single bad day so far.',
+ 'Do what you love, love what you do, its time to start living!',
+ 'Be ambitious. Keep your priorities straight, your mind right and your head up.',
+ 'This is your life, it hurts a whole fucking lot sometimes, but its your life, and its beautiful.',
+ 'It is possible to commit no mistakes and still lose. That is not a weakness, that is life.',
+ 'Be the person you needed when you were younger.',
+ 'No one will thank you for working yourself to death.',
+ 'A ship in harbor is safe, but that is not what ships are built for.',
+ 'Never make someone a priority when all you are to them is an option.',
+ 'Success isn\'t owned, it\'s leased, and the rent is due everyday.',
+ 'Problems end up in the the hospital, prison or the morgue. Everything else is just an inconvenience.',
+ 'Don\'t Panic.',
+ 'Always strive to be a better person than those who have wronged you.',
+ 'Life begins outside of your comfort zone.',
+ 'Be the Light in the Darkness.',
+ 'I will find my way, or make it myself.',
+]
+
+print(random.choices(MESSAGES)[0])
